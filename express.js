350edd40c63ed18ce4b15d2a91dd57ce6c2c7a88
suryapeddireddy <suryapeddireddy1203@gmail.com>
2025-03-26 23:43:51 +0530

    revised basic express

---
+++ express.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app=express();
+app.get('/',(req,res)=>{
+res.send('hello world');
+})
+app.listen(3000,()=>{
+console.log('server is running on port 3000');
+})
